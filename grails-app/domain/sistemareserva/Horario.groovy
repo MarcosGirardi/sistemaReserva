30b004ea004bfc3ccf54930c873396da146085c6
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-06-16 13:55:07 -0300

    tabulado

--- grails-app/domain/sistemareserva/Horario.groovy
+++ grails-app/domain/sistemareserva/Horario.groovy
@@ -4,7 +4,7 @@ class Horario {
   String  hora
 
   static  hasMany = [horaEntregas: Reserva, horaRetiros: Reserva]
-  static mappedBy = [horaEntregas: "horaEntrega", horaRetiros: "horaRetiro"]
+  static  mappedBy = [horaEntregas: "horaEntrega", horaRetiros: "horaRetiro"]
 
     static constraints = {
       hora  (nullable:true, unique:true)
