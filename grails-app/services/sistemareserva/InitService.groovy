85345cf257ebf25a08d56d0e4a10e09331f55d26
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-06-16 15:20:17 -0300

    BackupAdmin agregado

--- grails-app/services/sistemareserva/InitService.groovy
+++ grails-app/services/sistemareserva/InitService.groovy
@@ -6,17 +6,37 @@ import grails.transaction.Transactional
 class InitService {
 
     def init() {
-      log.println("iniciando")
-
-      def tipo = new TipoUsuario()
+      log.println("iniciando...")
+      def sudo = new SUser()
+      def tSudo = new TipoUsuario()
       def tC = TipoUsuario.createCriteria()
       def tU = tC {
         ilike("tipo", "su%")
       }
 
       if (!SUser.findAll()){
-        def sudo = new SUser()
+        log.println("no hay sudo")
+        sudo.nombre = "sudo"
+        sudo.password = "admin"
+        sudo.save(flush:true)
+        log.println("se creo sudo")
+
+        if(!TipoUsuario.findByTipoIlike("su%")){
+          log.println("no hay tipo sudo")
+          tSudo.tipo = "super usuario"
+          tSudo.save(flush:true)
+          log.println("se creo tipo sudo")
+        }
+
+        log.println("correcciones")
+        sudo = SUser.findAll()
+        tU = TipoUsuario.findByTipo("super usuario")
+        sudo[0].tipoUsuario = tU
+        tU.creador = sudo[0]
+        log.println("fin correcciones")
+
       }
+      log.println("fin inicializacion")
 
     }
 }
